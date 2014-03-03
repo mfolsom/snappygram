92acd96bcfbeb5b834ffee4f0ace6b56ea6e3d96
Megan Folsom <mfolsom@gmail.com>
2014-03-03 12:43:43 +0000

    Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snappygram::Application.config.secret_key_base = '07dfe353dd3baacc31113c18bffc7e4f886c5f27b29f33284c291c853db644a86534b6b486d1fec33b8839ef3d35d7f38410795a1a522d5c695984a064c03054'
