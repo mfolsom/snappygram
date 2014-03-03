a399fdb99eda4329b122b5c6dab5caf74276655a
Megan Folsom <mfolsom@gmail.com>
2014-03-03 16:14:49 +0000

    cleanup last commit

--- features/step_definitions/photo_steps.rb
+++ features/step_definitions/photo_steps.rb
@@ -1,6 +1,5 @@
 Given(/^I upload a new photo$/) do
   visit snaps_new_path
-  puts File.join(File.dirname(__FILE__), "../images")
   attach_file("snap[image]", File.join(File.dirname(__FILE__), "../images/ave_page_load.png"))
   click_button 'Upload Snap' 
 end
