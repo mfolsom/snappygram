29a176e9923b7a7e5cd2cc7dc7c03cae0cbac09a
biwek <biweks@gmail.com>
2014-03-05 10:39:55 +0000

    added username field to signup

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -1,6 +1,7 @@
 Given(/^I register as a new user$/) do
   visit 'users/sign_up'
   fill_in('user[email]', :with => 'example@example.com')
+  fill_in('user[username]', :with => 'exampleuser')
   fill_in('user[password]', :with => 'password')
   fill_in('user[password_confirmation]', :with => 'password')
   click_button 'Sign up'
